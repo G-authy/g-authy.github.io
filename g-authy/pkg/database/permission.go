b584af689895d641f194ce9b5ad7404c4279bb28
Muhammed abdelkader <muhammed.abdelkader@schibsted.com>
2023-12-25 22:06:06 +0100

    fix: Permission table , and extend swagger

--- g-authy/pkg/database/permission.go
+++ g-authy/pkg/database/permission.go
@@ -9,9 +9,11 @@ type Permission struct {
 	User       User      `gorm:"foreignkey:UserID" json:"-"`
 	GroupID    uuid.UUID `gorm:"type:uuid;not null" json:"-"`
 	Group      Group     `gorm:"foreignkey:GroupID" json:"-"`
-	RoleID     uuid.UUID `gorm:"type:uuid;not null" json:"-"`
+	RoleID     int       `gorm:"type:int;autoIncrement;primaryKey"`
 	Role       Role      `gorm:"foreignkey:RoleID" json:"role"`
 	VaultID    uuid.UUID `gorm:"type:uuid;not null" json:"-"`
 	Vault      Vault     `gorm:"foreignkey:VaultID" json:"-"`
+	SecretID   uuid.UUID `gorm:"type:uuid;not null" json:"-"`
+	Secret     Secret    `gorm:"foreignkey:SecretID" json:"-"`
 	Permission string    `gorm:"not null" json:"permission"`
 }
