056f7a9e79cb7e091211e83f70d03f3ca0313820
Muhammed.m.Abdelkader <muhammed.m.abdelkader@gmail.com>
2023-12-25 17:12:41 +0100

    creating RBAC updated adding group to tanent

--- g-authy/migrations/default_model_migration.go
+++ g-authy/migrations/default_model_migration.go
@@ -25,6 +25,7 @@ type Tenant struct {
 	DefaultModel
 	Region  string    `gorm:"not null" json:"region"`
 	Users   []User    `gorm:"foreignkey:TenantID" json:"-"`
+	Groups  []Group   `gorm:"foreignkey:GroupID" json:"-"`
 	OwnerID uuid.UUID `gorm:"foreignkey:UserID;not null;type:uuid"`
 }
 
